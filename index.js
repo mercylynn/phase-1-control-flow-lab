1ba9742d2b7d5c176ea253df6f741a866bb2ab9a
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-16 22:23:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride<=400){
+    return "This one is on me!";
+  }else if(ride>2000 && ride<2500){
+    return "I will gladly take your thirty bucks.";
+
+  }
+  else if(ride>2500)
+  
+  {return "No can do."}
+}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+function ternaryCheckCity(city){
+  // Write your code here!
+  return((city==="NYC")?"Ok, sounds good.":"No go.")
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case "not as generous":
+      return "Thank you.";
+      case "generous":
+        return"Thank you so much.";
+        default:
+          return"Bye.";
+  }
 }
